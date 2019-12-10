71f722ff73a1a4ab5644e2587fea11508318815d
susilahandika <susilaandika@gmail.com>
2019-12-10 20:02:07 +0800

    delete data 10122019

--- resources/views/kategori/index.blade.php
+++ resources/views/kategori/index.blade.php
@@ -29,13 +29,12 @@
                                 <td>{{ $a->updated_at }}</td>
                                 <td>
                                     <a href="{{ route('kategori.edit', $a->id) }}" class="btn btn-warning btn-sm">Ubah</a>
-                                    {{-- <a href="{{ route('kategori.destroy', $a->id) }}" class="btn btn-danger btn-sm">Hapus</a> --}}
-
-                                    <a href="#" class="btn btn-danger btn-sm" onclick="run_delete({{ $a->id }})">Hapus</a>
+                                    <a href="{{ route('kategori.destroy', $a->id) }}" class="btn btn-danger btn-sm">Hapus</a>
 
                                     {{-- <form action="{{ route('kategori.destroy', $a->id) }}" method="post">
                                         {{ csrf_field() }}
                                         {{ method_field('DELETE') }}
+
                                         <input type="submit" value="Hapus" class="btn btn-danger btn-sm">
                                     </form> --}}
                                 </td>
